021e9a324dda63459a4b11436c5e01522d820bbc
lukemodular <lukas.truniger@gmail.com>
2015-05-19 17:14:31 +0200

    added dejaMaster and dejaNode

---
+++ dejaMaster_0.1/dejaMaster_0.1.ino
@@ -0,0 +1,95 @@
+//________________________________________________________________
+// dejaEntendu master
+
+#include <FlexCAN.h>
+
+byte nodes = 2;
+byte led = 13;
+FlexCAN CANbus(1000000);
+static CAN_message_t msg;
+char frameBuf[1024];
+byte x_msg;
+byte y_msg;
+byte c_msg;
+
+
+// -------------------------------------------------------------
+void setup(void)
+{
+  Serial.begin(230400);
+  CANbus.begin();
+  pinMode(led, OUTPUT);
+  delay(500);
+}
+
+
+// -------------------------------------------------------------
+void loop(void) {
+
+  while (Serial.available()) {
+    digitalWrite(led, 1);
+    Serial.readBytes(frameBuf, 1024);
+    msg.len = 8;  //CAN frame with up to 8 bytes
+    int j = 0;
+    for (int i = 0; i < 1024; i++) {
+      if (j < 7) {
+        msg.buf[j] = '0' + frameBuf[i];
+        j++;
+      }
+      else
+      {
+        msg.buf[j] = '0' + frameBuf[i];
+        for (int idn = 0; idn < nodes; ++idn ) {
+          msg.id = idn;
+          CANbus.write(msg);
+          delay(2);
+        }
+        j = 0;
+      }
+      //      Serial.print("x_msg: ");
+      //      Serial.println(msg.buf[0]);
+      //      Serial.print("y_msg: ");
+      //      Serial.println(msg.buf[1]);
+      //      Serial.print("c_msg: ");
+      //      Serial.println(msg.buf[2]);
+    }
+    digitalWrite(led, 0);
+  }
+
+}
+
+
+
+/*
+ if (canMetro.check()) {
+
+ // insert a time delay between transmissions
+ if ( !txTimer ) {
+
+ txTimer = 50;//milliseconds, shorter???
+ msg.len = 8;   //longer??
+ for (int idn=0; idn<nodes; ++idn ) {
+ msg.id = idn;
+ for( int idb=0; idb<msg.len; ++idb ) {
+ //better message!!
+ msg.buf[idb] = '0'+idb;
+ }
+ }
+
+
+ // send 6 at a time to force tx buffering
+ txCount = 8;
+ digitalWrite(led, 1);
+ Serial.println(".");
+ while ( txCount-- ) {
+ CANbus.write(msg);
+ msg.buf[0]++;
+ }
+ digitalWrite(led, 0);
+ // time delay to force some rx data queue use
+ rxTimer = 3;//milliseconds
+ }
+
+ }
+ */
+
---
+++ dejaNode_0.1/dejaNode_0.1.ino
@@ -0,0 +1,332 @@
+//________________________________________________________________
+//dejaEntendu node
+
+
+/*TODO:
+ -Protocoll Parameter!!!
+ (globalLight, globalVolume)
+ -pixel Buffer??!?
+
+ */
+
+#include <SmartMatrix_32x32.h>
+#include <Metro.h>
+#include <FlexCAN.h>
+#include <EEPROM.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+SmartMatrix matrix;
+
+//_______________________________________________________________
+//Timing
+Metro sysTimer = Metro(1);// milliseconds
+unsigned int canTimer, ledTimer, monitorTimer, noteOnTimer, noteOffTimer;
+
+//_______________________________________________________________
+//variables CAN
+byte led = 13;
+int maskID = 0xFFF;      //bit filtering of filterID's 0x000 opens to all messages, 0xFFF only to the messages specified
+int filterID;
+
+// Our adress we listen to
+byte filterid_in_eeprom = 0;
+FlexCAN CANbus(1000000);
+static CAN_message_t rxmsg;
+static CAN_filter_t mask;
+static CAN_filter_t filter;
+static uint8_t hex[17] = "0123456789abcdef";
+byte x_msg;
+byte y_msg;
+byte c_msg;
+byte red;
+byte green;
+byte blue;
+byte frameBuf[1024];
+byte test;
+int bufCount = 0;
+int pxlCount = 0;
+
+//________________________________________________________________
+//variables smartMatrix
+
+//const int defaultBrightness = 100*(255/100);    // full brightness
+const int defaultBrightness = 15 * (255 / 100); // dim: 15% brightness
+const rgb24 defaultBackgroundColor = {
+  0, 0, 0
+};
+byte colorRand;
+
+//________________________________________________________________
+//variables Audio
+
+AudioSynthWaveform       waveform1;
+AudioSynthWaveformSineModulated fm1;
+AudioEffectEnvelope      envelope1;
+AudioOutputAnalog        out;
+AudioConnection          patchCord1(waveform1, envelope1);
+//AudioConnection          patchCord2(fm1, envelope1);
+AudioConnection          patchCord2(envelope1, out);
+int noteLength;
+
+
+//______________________________________________________________
+//hexDum
+
+//static void hexDump(uint8_t dumpLen, uint8_t *bytePtr)
+//{
+//  uint8_t working;
+//  while ( dumpLen-- ) {
+//    working = *bytePtr++;
+//    Serial.write( hex[ working >> 4 ] );
+//    Serial.write( hex[ working & 15 ] );
+//  }
+//  Serial.write('\r');
+//  Serial.write('\n');
+//}
+
+
+// -------------------------------------------------------------
+void setup() {
+
+  Serial.begin(115200);
+
+
+  //___________________________________
+  //SETUP CAN
+
+  // Pull filter address out of eeprom
+  filterID = EEPROM.read(filterid_in_eeprom);
+  mask.id = maskID;
+  filter.id = filterID;
+  CANbus.begin(mask);
+  CANbus.setFilter(filter, 0);
+  CANbus.setFilter(filter, 1);
+  CANbus.setFilter(filter, 2);
+  CANbus.setFilter(filter, 3);
+  CANbus.setFilter(filter, 4);
+  CANbus.setFilter(filter, 5);
+  CANbus.setFilter(filter, 6);
+  CANbus.setFilter(filter, 7);
+  pinMode(led, OUTPUT);
+  //digitalWrite(led, 1);
+
+  delay(2000);
+  Serial.println(F("Hello Teensy 3.1 CAN Test."));
+  Serial.print("filteID: ");
+  Serial.println(filterID, HEX);
+
+  //____________________________________
+  //SETUP smartMatrix
+  // initialize the digital pin as an output.
+
+  matrix.begin();
+  matrix.setBrightness(defaultBrightness);
+  matrix.setColorCorrection(cc24);
+  //matrix.setRotation(rotation180);
+
+
+  //____________________________________
+  //SETUP audio
+  AudioMemory(12);
+
+  waveform1.pulseWidth(0.1);
+  //waveform1.arbitraryWaveform(array, 1000);
+  waveform1.begin(0.1, 175, WAVEFORM_SINE);
+  //Waveforms: WAVEFORM_SINE, WAVEFORM_SQUARE, WAVEFORM_SAWTOOTH, WAVEFORM_TRIANGLE, WAVEFORM_PULSE, WAVEFORM_ARBITRARY
+  //fm1.amplitude(0.5);
+  //fm1.frequency(200);
+  envelope1.attack(5);
+  envelope1.hold(0);
+  envelope1.decay(2);
+  envelope1.sustain(0.8);
+  envelope1.release(5);
+  //larger attack and release time leads to clicks (after ~100ms)
+
+  // Initialize processor and memory measurements
+  AudioProcessorUsageMaxReset();
+  AudioMemoryUsageMaxReset();
+
+
+  sysTimer.reset();
+
+}
+
+
+
+// -------------------------------------------------------------
+void loop() {
+
+
+  // service software timers based on Metro tick
+  if ( sysTimer.check() ) {
+    if ( canTimer ) {
+      --canTimer;
+    }
+    if ( ledTimer ) {
+      --ledTimer;
+    }
+    if ( monitorTimer ) {
+      --monitorTimer;
+    }
+    if ( noteOnTimer ) {
+      --noteOnTimer;
+    }
+    if ( noteOffTimer ) {
+      --noteOffTimer;
+    }
+  }
+
+
+
+  //_______________________________
+  //CAN
+
+  if (!canTimer) {
+    while ( CANbus.read(rxmsg) ) {
+        //hexDump( sizeof(rxmsg), (uint8_t *)&rxmsg );
+//             Serial.print("msg.id: ");
+//              Serial.println(rxmsg.id, HEX);
+//              Serial.print("msg.extended: ");
+//              Serial.println(rxmsg.ext);
+//              Serial.print("msg.len: ");
+//              Serial.println(rxmsg.len);
+//              Serial.print("msg.timeout: ");
+//              Serial.println(rxmsg.timeout);
+      for (int i = 0; i < 8; i++) {
+          frameBuf[bufCount] = rxmsg.buf[i] - 48;
+          bufCount++;
+        }
+      }
+    if (bufCount > 1023) {
+        bufCount = 0;
+      }
+    //    for (int i = 0; i < 1024; i++) {
+    //    frameBuf[i] = random(0, 2);
+    //  }
+    canTimer = 2;
+  }
+
+  //_________________________________
+  //audioNoteOn
+
+  if (!noteOnTimer) {
+    waveform1.frequency(175 * random(1, 4));
+    //waveform1.amplitude(0.3*random(1, 5));
+    envelope1.noteOn();
+    //noteLength = random(500, 4000);
+    noteLength = 100;
+    noteOnTimer = 2000;        //note happend???
+    noteOffTimer = noteLength;
+  }
+
+  //_________________________________
+  //smartMatrix
+
+  if (!ledTimer) {
+    int pxlCount = 0;
+    matrix.swapBuffers(true);
+    //matrix.fillScreen(defaultBackgroundColor);
+    //matrix.swapBuffers(true);
+    test = frameBuf[0];
+    for (int x = 0; x < 32; x++) {
+      for (int y = 0; y < 32; y++) {
+        if (frameBuf[pxlCount] == 1) {
+          c_msg = 255;
+          green = map(c_msg, 0, 255, 0, 230);
+          blue = map(c_msg, 0, 255, 0, 240);
+          matrix.drawPixel(x, y, {
+            c_msg, green, blue
+          }
+                          );
+
+        }
+        if (frameBuf[pxlCount] == 0)  {
+          c_msg = 0;
+          green = map(c_msg, 0, 255, 0, 230);
+          blue = map(c_msg, 0, 255, 0, 240);
+          matrix.drawPixel(x, y, {
+            c_msg, green, blue
+          }
+                          );
+
+        }
+        pxlCount++;
+      }
+    }
+    ledTimer = 20;
+  }
+
+  //_________________________________
+  //audioNoteOff
+
+  if (!noteOffTimer) {
+    envelope1.noteOff();
+    noteOffTimer = 2000;
+    noteOnTimer = 1000;
+  }
+
+}
+
+
+
+
+
+//
+//
+//  //___________________________
+//  //System Monitor
+//
+//  if (!monitorTimer) {
+//    Serial.print("Proc = ");
+//    Serial.print(AudioProcessorUsage());
+//    Serial.print(" (");
+//    Serial.print(AudioProcessorUsageMax());
+//    Serial.print("),  Mem = ");
+//    Serial.print(AudioMemoryUsage());
+//    Serial.print(" (");
+//    Serial.print(AudioMemoryUsageMax());
+//    Serial.println(")");
+//    monitorTimer = 5000;
+//  }
+
+
+
+
+
+
+//  if (ledMetro2.check()) {
+//
+////        matrix.setScrollColor({0xff, 0xff, 0xff});
+////        matrix.setScrollMode(wrapForward);
+////        matrix.setScrollSpeed(20);
+////        matrix.setScrollOffsetFromEdge(y_msg);
+////
+////        matrix.setScrollFont(font3x5);
+////        matrix.scrollText("DEJA", 1);
+////        while (matrix.getScrollStatus());
+//
+//    matrix.swapBuffers(true);
+//    //    matrix.drawChar(x_msg, y_msg, {
+//    //      0xff, 0xff, 0xff        }
+//    //    , 'A');
+//    matrix.drawString(x_msg-8, y_msg-8, {
+//      0xff, 0xe6, 0xf0           }
+//    , "DEJA");
+//
+//  }
+
+
+
+//    for (int x = 0; x < 32; x++) {
+//      for (int y = 0; y < 32; y++) {
+//        colorRand = random(0, 255);
+//        matrix.drawPixel(x_msg, y_msg, {
+//          colorRand, colorRand, colorRand        }
+//        );
+//      }
+//    };
+
+
