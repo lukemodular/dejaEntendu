c6b98a3bc13449fe7b1c966863a9e3d3eb022754
lukemodular <lukas.truniger@gmail.com>
2015-05-19 17:00:20 +0200

    minor changes in example, still error without ligne 49

--- textResultionTest/textResultionTest.ino
+++ textResultionTest/textResultionTest.ino
@@ -4,7 +4,7 @@
 
 SmartMatrix matrix;
 
-const int defaultBrightness = 50*(255/100);    // full brightness
+const int defaultBrightness = 50 * (255 / 100); // full brightness
 //const int defaultBrightness = 15*(255/100);    // dim: 15% brightness
 const int defaultScrollOffset = 6;
 const rgb24 defaultBackgroundColor = {0, 0, 0};
@@ -16,58 +16,45 @@ const int ledPin = 13;
 
 
 void setup() {
-  
-    pinMode(ledPin, OUTPUT);
 
-    Serial.begin(38400);
+  pinMode(ledPin, OUTPUT);
 
-    matrix.begin();
-    matrix.setBrightness(defaultBrightness);
-    matrix.setScrollOffsetFromEdge(defaultScrollOffset);
-    matrix.setColorCorrection(cc24);
-    
+  Serial.begin(38400);
+
+  matrix.begin();
+  matrix.setBrightness(defaultBrightness);
+  matrix.setScrollOffsetFromEdge(defaultScrollOffset);
+  matrix.setColorCorrection(cc24);
+
 }
 
 
 void loop() {
-    int i, j;
-    
-    char text[28] = "HELLO DEJA ENTENDU IS NEAR";
-    //char text[28] = "Hello deja entendu is Near";
-    
-    // clear screen
-    matrix.fillScreen(defaultBackgroundColor);
-    matrix.swapBuffers(true);
+  int i, j;
 
+  char text[28] = "HELLO DEJA ENTENDU IS NEAR";
+  //char text[28] = "Hello deja entendu is Near";
 
-    // font options
-      
-        matrix.swapBuffers(true);
-        matrix.fillScreen({0, 0, 0});
-        matrix.swapBuffers(true);
-        r = random(220, 255);
-        g = random(220, 255);
-        b = random(220, 255);
-        letter = text[i];            //?????doen't work without this line
-    for(int y = 0; y < 32; y=y+4) {
-      for(int x = 0; x < 32; x=x+4){
-        matrix.setFont(font4x4);
-        //matrix.setFont(font4x8_ansi);
-        matrix.drawChar(x, y, {r, g, b}, text[i]);  //random(65, 90) 90, 122
-        matrix.swapBuffers(true);
-        delay(100);
-        i++;
-        if(i > 28) i = 0;
+  matrix.setFont(font4x4);
+  //matrix.setFont(font4x8_ansi);
+  //custom fonts: font4x4, font4x8_ansi, font4x8
+  //library fonts: font3x5, font5x7, font6x10, font8x13  
+
+  matrix.fillScreen(defaultBackgroundColor);
+  matrix.swapBuffers(true);
+
+  r = random(220, 255);
+  g = random(220, 255);
+  b = random(220, 255);
+  letter = text[i];            //?????doen't work without this line
+  for (int y = 0; y < 32; y = y + 4) {
+    for (int x = 0; x < 32; x = x + 4) {
+      matrix.drawChar(x, y, {r, g, b}, text[i]);  //random(65, 90) 90, 122
+      matrix.swapBuffers(true);
+      delay(100);
+      i++;
+      if (i > 28) i = 0;
     }
-    }
-        delay(10000);
-        
-        //different fonts
-        //matrix.setFont(font4x4);
-        //matrix.setFont(font4x8_ansi);
-        //matrix.setFont(font4x8);
-        //matrix.setFont(font5x7);
-        //matrix.setFont(font6x10);
-        //matrix.setFont(font8x13);
-
+  }
+  delay(10000);
 }
